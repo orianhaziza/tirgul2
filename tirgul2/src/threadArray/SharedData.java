d6f1f5d71df0505692f0360e2a691dbaa3d2fb45
classroom <classroom@M102-8.brdeng.ac>
2024-06-06 11:01:14 +0300

    added documentation

--- tirgul2/src/threadArray/SharedData.java
+++ tirgul2/src/threadArray/SharedData.java
@@ -1,7 +1,10 @@
 package threadArray;
 
 import java.util.ArrayList;
-
+/**a class that fix the whole data
+ * 
+ *
+ */
 public class SharedData 
 {
 	private ArrayList<Integer> array;;
@@ -9,37 +12,66 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * a constructor for the class
+	 * @param array
+	 * @param b
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * a method to return the winning array
+	 * @return the winning array saved in winArray
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * a method to keep the winning array
+	 * @param winArray the array that needs to be saved in this.winArray
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * a method to return array
+	 * @return array
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * a method to return b
+	 * @return b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * a method to return flag
+	 * @return flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * a method to set the flag
+	 * @param flag the object that needs to be kept in flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
